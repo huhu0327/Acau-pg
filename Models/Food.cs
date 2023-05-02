faa5cbdd39654ba89446248a28da4909695e3a7e
Dareum Nam <ndr.developer@gmail.com>
2023-05-02 21:17:00 +0900

    new project

---
+++ Models/Food.cs
@@ -0,0 +1,17 @@
+﻿namespace Acau_Playground.Models
+{
+    public class Food
+    {
+        public string Type { get; set; }
+        public IEnumerable<Content> Contents { get; set; }
+    }
+
+    public class Content
+    {
+        public string Name { get; set; }
+        public int ShopPrice { get; set; }
+        public int PurchasePrice { get; set; }
+        public int SellPrice { get; set; }
+    }
+
+}
