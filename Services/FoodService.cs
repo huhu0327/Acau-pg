437d22b2b7c8095ef20d37c8919283a55246a7ca
Dareum Nam <ndr.developer@gmail.com>
2023-05-04 04:37:25 +0900

    업데이트

---
+++ Services/FoodService.cs
@@ -0,0 +1,26 @@
+﻿using Acau_Playground.Models;
+using System.Net.Http.Json;
+
+namespace Acau_Playground.Services
+{
+    public interface IFoodService
+    {
+        public Task<IReadOnlyList<Food>?> GetFoodAsync();
+    }
+
+    public class FoodService : IFoodService
+    {
+        private readonly HttpClient _httpClient;
+
+        public FoodService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<IReadOnlyList<Food>?> GetFoodAsync()
+        {
+            return _httpClient.GetFromJsonAsync<IReadOnlyList<Food>?>("sample-data/datas.json");
+        }
+
+    }
+}
---
+++ Viewmodels/FoodViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Acau_Playground.Models;
+using Acau_Playground.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Acau_Playground.Viewmodels
+{
+    public class FoodViewModel : ObservableObject
+    {
+        private readonly IFoodService _foodService;
+
+        public IReadOnlyList<Food>? Foods;
+
+        public FoodViewModel(IFoodService foodService)
+        {
+            _foodService = foodService;
+        }
+
+        public async Task LoadDataAsync()
+        {
+            Foods = await _foodService.GetFoodAsync();
+        }
+
+        public IEnumerable<Content> GetContents(int index)
+        {
+            if (Foods is null) return Enumerable.Empty<Content>();
+
+            return Foods[index].Contents;
+        }
+
+    }
+}
